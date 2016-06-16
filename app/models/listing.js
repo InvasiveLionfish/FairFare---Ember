aed60047841fda6f97e606cc176344b6577bba5e
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2016-06-16 17:59:59 -0400

    Api setup for index and bootstrap

--- app/models/listing.js
+++ app/models/listing.js
@@ -1,8 +1,9 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
-// import { belongsTo, hasMany } from 'ember-data/relationships';
+import { belongsTo} from 'ember-data/relationships';
 
 export default Model.extend({
   pickupTime: attr('date'),
-  description: attr('string')
+  description: attr('string'),
+  donor: belongsTo('donor')
 });
