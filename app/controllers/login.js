4e14e9d5323c5d5b31704b5759b39124587b2fac
kwebster2 <kevin.r.webster@gmail.com>
2016-06-19 19:08:40 -0400

    added logic to display logout link

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,12 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  session: Ember.inject.service(),
+  session: Ember.inject.service('session'),
 
   actions: {
     authenticate: function(credentials) {
       var authenticator = 'authenticator:jwt';
       this.get('session').authenticate(authenticator, credentials).catch((reason)=>{
+        debugger;
         this.set('errorMessage', reason.responseJSON.errors || reason);
       }).then(()=>{
         this.transitionToRoute('listings');
