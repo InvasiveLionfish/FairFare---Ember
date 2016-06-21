a510550ae4381ab1c5d837616e61d343845355d7
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2016-06-20 23:19:59 -0400

    Sorting works

--- app/controllers/listings.js
+++ app/controllers/listings.js
@@ -4,7 +4,20 @@ export default Ember.Controller.extend({
   sessionAccount: Ember.inject.service('session-account'),
   isShowingModal: false,
   query: null,
-  displayListings: Ember.computed.reads('model'),
+  displayListings: Ember.computed('model', 'query', function(){
+    const query = this.get('query');
+    const model = this.get('model');
+    if (!query){
+      return model;
+    } else {
+      model.forEach((listing) => {
+        const match = `${listing.get('description')} ${listing.get('donor.name')}`;
+        const score = match.score(query);
+        listing.set('_score', score);
+      });
+      return model.filterBy('_score').sortBy('_score').reverse();
+    }
+  }),
   actions: {
     toggleModal: function() {
       this.toggleProperty('isShowingModal');
