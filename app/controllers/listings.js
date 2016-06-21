7d6cfece6dd7a7ebb0346145923706cf023656d9
InvasiveLionfish <InvasiveLionfish@users.noreply.github.com>
2016-06-20 22:59:04 -0400

    String Score Added

--- app/controllers/listings.js
+++ app/controllers/listings.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   sessionAccount: Ember.inject.service('session-account'),
   isShowingModal: false,
+  query: null,
+  displayListings: Ember.computed.reads('model'),
   actions: {
     toggleModal: function() {
       this.toggleProperty('isShowingModal');
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,6 +10,9 @@ module.exports = function(defaults) {
       'importBootstrapCSS': true
     }
   });
+  
+  app.import('bower_components/string_score/string_score.min.js')
+ 
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
